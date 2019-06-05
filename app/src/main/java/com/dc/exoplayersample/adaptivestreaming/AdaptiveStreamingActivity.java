19a2aacc7b88d08684cdcf67deb2292b051666c4
DC <cdebayan09@gmail.com>
2019-06-05 23:51:44 +0530

    api change for adaptive stream

--- app/src/main/java/com/dc/exoplayersample/adaptivestreaming/AdaptiveStreamingActivity.java
+++ app/src/main/java/com/dc/exoplayersample/adaptivestreaming/AdaptiveStreamingActivity.java
@@ -1,10 +1,8 @@
 package com.dc.exoplayersample.adaptivestreaming;
 
 import android.net.Uri;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
 import com.dc.exoplayersample.R;
 import com.google.android.exoplayer2.DefaultLoadControl;
@@ -26,6 +24,7 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
 
     private PlayerView simpleExoPlayerView;
     private SimpleExoPlayer player;
+    String hls_url = "https://playertest.longtailvideo.com/adaptive/bbbfull/bbbfull.m3u8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,26 +41,13 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
                 new DefaultRenderersFactory(AdaptiveStreamingActivity.this),
                 new DefaultTrackSelector(adaptiveTrackSelection),
                 new DefaultLoadControl());
-
-        //init the player
         simpleExoPlayerView.setPlayer(player);
-
-        //-------------------------------------------------
-        DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
-        // Produces DataSource instances through which media data is loaded.
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(AdaptiveStreamingActivity.this,
-                Util.getUserAgent(AdaptiveStreamingActivity.this, "Exo2"), defaultBandwidthMeter);
-
-        //-----------------------------------------------
-        //Create media source
-        String hls_url = "http://playertest.longtailvideo.com/adaptive/bbbfull/bbbfull.m3u8";
-        Uri uri = Uri.parse(hls_url);
-        Handler mainHandler = new Handler();
-        //MediaSource mediaSource = new HlsMediaSource(uri, dataSourceFactory, mainHandler, null);
-        MediaSource mediaSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                this,
+                Util.getUserAgent(this, "exo-player"),
+                new DefaultBandwidthMeter());
+        MediaSource mediaSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(hls_url));
         player.prepare(mediaSource);
-
-
         player.setPlayWhenReady(true);
     }
 
