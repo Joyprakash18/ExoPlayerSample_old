d7e1a43d3446ec51736ba8743278038de68a6bd0
DC <cdebayan09@gmail.com>
2019-06-11 18:21:20 +0530

    adaptive stream video quality control

--- app/src/main/java/com/dc/exoplayersample/adaptivestreaming/AdaptiveStreamingActivity.java
+++ app/src/main/java/com/dc/exoplayersample/adaptivestreaming/AdaptiveStreamingActivity.java
@@ -3,28 +3,41 @@ package com.dc.exoplayersample.adaptivestreaming;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.dc.exoplayersample.R;
 import com.dc.exoplayersample.video.VideoPlayerActivity;
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.FixedTrackSelection;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class AdaptiveStreamingActivity extends AppCompatActivity {
 
@@ -34,6 +47,10 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
     private ProgressBar progress;
     private boolean isPlaying = true;
     private ImageView playPause;
+    private DefaultTrackSelector defaultTrackSelector;
+    private ArrayList<Format> qualityList;
+    private ImageView setting;
+    private TrackGroupArray trackGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +64,10 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
 
     private void initializePlayer() {
         TrackSelection.Factory adaptiveTrackSelection = new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter());
+        defaultTrackSelector = new DefaultTrackSelector(adaptiveTrackSelection);
         player = ExoPlayerFactory.newSimpleInstance(
                 new DefaultRenderersFactory(AdaptiveStreamingActivity.this),
-                new DefaultTrackSelector(adaptiveTrackSelection),
+                defaultTrackSelector,
                 new DefaultLoadControl());
         simpleExoPlayerView.setPlayer(player);
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
@@ -69,18 +87,33 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
                         progress.setVisibility(View.VISIBLE);
                         break;
                     case Player.STATE_ENDED:
-                            player.seekTo(0);
+                        player.seekTo(0);
                         break;
                     case Player.STATE_IDLE:
                         break;
                     case Player.STATE_READY:
                         progress.setVisibility(View.GONE);
+                        getVideoQuality();
                         break;
                     default:
                         break;
                 }
             }
         });
+
+
+    }
+
+    private void getVideoQuality() {
+        if (defaultTrackSelector.getCurrentMappedTrackInfo() != null) {
+            qualityList = new ArrayList<>();
+            // 0 => video 1 => Audio
+            trackGroups = defaultTrackSelector.getCurrentMappedTrackInfo().getTrackGroups(0);
+            for (int i = 0; i < trackGroups.get(0).length; i++) {
+                Format quality = trackGroups.get(0).getFormat(i);
+                qualityList.add(quality);
+            }
+        }
     }
 
     private void setPlayPause() {
@@ -101,6 +134,35 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
                 setPlayPause();
             }
         });
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (qualityList != null && qualityList.size() > 0) {
+                    PopupMenu popupMenu = new PopupMenu(AdaptiveStreamingActivity.this, setting);
+                    popupMenu.getMenu().add(1, 0, 1, "Auto");
+                    for (int i = 0; i < qualityList.size(); i++) {
+                        popupMenu.getMenu().add(1, i + 1, i + 2, String.valueOf(qualityList.get(i).height));
+                    }
+                    popupMenu.show();
+
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            if (item.getItemId() == 0) {
+                                Toast.makeText(AdaptiveStreamingActivity.this, "auto", Toast.LENGTH_SHORT).show();
+                            } else {
+                                int bitrate = qualityList.get(item.getItemId() - 1).bitrate;
+                                defaultTrackSelector.setParameters(defaultTrackSelector.buildUponParameters()
+                                        .setMaxVideoBitrate(bitrate)
+                                        .build()
+                                );
+                            }
+                            return false;
+                        }
+                    });
+                }
+            }
+        });
 
     }
 
@@ -108,5 +170,6 @@ public class AdaptiveStreamingActivity extends AppCompatActivity {
         simpleExoPlayerView = findViewById(R.id.simpleExoPlayerView);
         progress = simpleExoPlayerView.findViewById(R.id.progress);
         playPause = simpleExoPlayerView.findViewById(R.id.playPause);
+        setting = simpleExoPlayerView.findViewById(R.id.setting);
     }
 }
